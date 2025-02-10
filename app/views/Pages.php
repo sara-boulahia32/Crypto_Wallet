7bd2f1eb3123d7a56cf6f4773c781b01437d1ada
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-02-10 21:15:54 +0100

    consume the api

--- app/views/Pages.php
+++ app/views/Pages.php
@@ -1,1 +1,15 @@
-<?php
+ <ul id="coin-list">
+        <?php foreach ($data['data']['data'] as $coin): ?>
+            <li>
+                <strong><?php echo htmlspecialchars($coin['name']); ?></strong>:
+                <br/>
+                <strong>slug:<?php echo htmlspecialchars($coin['slug']); ?></strong>:
+                <br/>
+                <strong>symbol:<?php echo htmlspecialchars($coin['symbol']); ?></strong>:
+                <br/>
+                <strong>max_supply:<?php echo $coin['max_supply']; ?></strong>:
+                <br/>
+                <strong>prix :</strong><?php echo '$' . number_format($coin['quote']['USD']['price'], 2); ?>
+            </li>
+        <?php endforeach; ?>
+ </ul>
