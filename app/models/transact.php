aee980cc43aca05aee995a10fc9e24794608cd0c
sara boulahia <saraboulahia32@gmail.com>
2025-02-12 16:54:03 +0100

    methods into model transaction

---
+++ app/models/transact.php
@@ -0,0 +1,35 @@
+<?php
+class transact {
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = new Database;
+    }
+    public function buy_transac($data){
+        $this->conn->query("INSERT INTO transaction (sender_id,crypto_id,montant) VALUES (:sender,:crypto_id,:amount)");
+        $this->conn->bind(':sender', 1);
+        $this->conn->bind(':crypto_id', $data['cryptoid']);
+        $this->conn->bind(':amount', $data['cryptoamount']);
+      
+        $this->conn->execute();
+    }
+    public function sell_transac($data){
+        $this->conn->query("INSERT INTO transaction (sender_id,crypto_id,montant) VALUES (:sender,:crypto_id,:amount)");
+        $this->conn->bind(':sender', 1);
+        $this->conn->bind(':crypto_id', $data['cryptoid']);
+        $this->conn->bind(':amount', $data['cryptoamount']);
+      
+        $this->conn->execute();
+    }
+    public function send_coin($data){
+        $this->conn->query("INSERT INTO transaction (sender_id,reciever,crypto_id,montant) VALUES (:sender,:reciever,:crypto_id,:amount)");
+        $this->conn->bind(':sender', 1);
+        $this->conn->bind(':reciever',$data['nexusid'] );
+        $this->conn->bind(':crypto_id', $data['cryptoid']);
+        $this->conn->bind(':amount', $data['coin_amount']);
+      
+        $this->conn->execute();
+    }
+    
+}
