355ffc287306caf6d982dede21957b0fae27fdd5
sara boulahia <saraboulahia32@gmail.com>
2025-02-13 15:31:55 +0100

    ajouter method

--- app/models/Wallet.php
+++ app/models/Wallet.php
@@ -8,7 +8,6 @@ class Wallet {
     }
 
     public function check_Qte_Sell($crypto_id,$amount){
-        // $this->conn->query("SELECT soldusdt, CASE WHEN soldusdt > 0 THEN true ELSE false END as result FROM portefeuille WHERE user_id = :user_id AND crypto_id = :crypto_id");
         $this->conn->query("SELECT soldusdt from portefeuille where user_id = :user_id and crypto_id = :crypto_id and soldusdt - :amount >= 0");
         $this->conn->bind(':user_id',1);
         $this->conn->bind(':crypto_id', $crypto_id);
@@ -21,6 +20,22 @@ class Wallet {
         
         
     }
+    public function check_Qte_Sel($crypto_id, $amount)
+{
+    $this->conn->query("SELECT soldusdt FROM portefeuille WHERE user_id = :user_id AND crypto_id = :crypto_id");
+
+    $this->conn->bind(':user_id', 1);
+    $this->conn->bind(':crypto_id', $crypto_id);
+    
+    $result = $this->conn->single(); 
+    
+    if ($result && $result->soldusdt >= $amount) {
+        return $result;
+    }
+
+    return false;
+}
+
     
     public function wallet_sell($data){
         $row=$this->check_Qte_Sell($data['cryptoid'],$data['cryptoamount']);
