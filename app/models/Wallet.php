cfa629cd19f392568d75e738fdc05c14fe7038f7
sara boulahia <saraboulahia32@gmail.com>
2025-02-13 12:59:21 +0100

    check wallet balance and depending on id crypto effectuer transaction sell

--- app/models/Wallet.php
+++ app/models/Wallet.php
@@ -8,25 +8,30 @@ class Wallet {
     }
 
     public function check_Qte_Sell($crypto_id,$amount){
-        $this->conn->query("SELECT qte,if(qte>=:amount,true,false) as result from wallet where user_id=:user_id AND crypto_id=:crypto_id");
+        // $this->conn->query("SELECT soldusdt, CASE WHEN soldusdt > 0 THEN true ELSE false END as result FROM portefeuille WHERE user_id = :user_id AND crypto_id = :crypto_id");
+        $this->conn->query("SELECT soldusdt from portefeuille where user_id = :user_id and crypto_id = :crypto_id and soldusdt - :amount >= 0");
         $this->conn->bind(':user_id',1);
         $this->conn->bind(':crypto_id', $crypto_id);
         $this->conn->bind(':amount', $amount);
-        $this->conn->execute();
-        return $this->conn->single();
+        if($this->conn->execute() && $this->conn->rowCount() > 0){
+            return true;
+        }else{
+            return false;
+        }
+        
+        
     }
     
     public function wallet_sell($data){
         $row=$this->check_Qte_Sell($data['cryptoid'],$data['cryptoamount']);
-        if($row->qte){
-        $data['cryptoamount']=$row->qte - $data['cryptoamount'];
+        if($row){
 
-        $this->conn->query("UPDATE  wallet set qte=:qte where user_id =:user_id AND crypto_id=:crypto_id ");
-        $this->conn->bind(':user_id', 1);
-        $this->conn->bind(':crypto_id', $data['cryptoid']);
-        $this->conn->bind(':qte', $data['cryptoamount']);
-        $this->conn->execute();
-        return true;
+            $this->conn->query("UPDATE  portefeuille set soldusdt = soldusdt - :qte where user_id =:user_id AND crypto_id=:crypto_id ");
+            $this->conn->bind(':user_id', 1);
+            $this->conn->bind(':crypto_id', $data['cryptoid']);
+            $this->conn->bind(':qte', $data['cryptoamount']);
+            $this->conn->execute();
+            return true;
         }else{
         return false;
         }
@@ -36,7 +41,7 @@ class Wallet {
 
 
     public function add_to_wallet($data){
-        $this->conn->query("SELECT qte,if(qte>0,true,false) from portefeuille where user_id=:user_id AND crypto_id=:crypto_id");
+        $this->conn->query("SELECT soldusdt, CASE WHEN soldusdt > 0 THEN true ELSE false END as result FROM portefeuille WHERE user_id=:user_id AND crypto_id=:crypto_id");
         $this->conn->bind(':user_id',1);
         $this->conn->bind(':crypto_id', $data['cryptoid']);
         $this->conn->execute();
@@ -44,11 +49,11 @@ class Wallet {
        
 
         if($row){
-            $data['cryptoamount']=$row->qte + $data['cryptoamount'];
+            $data['cryptoamount']=$row->soldusdt  + $data['cryptoamount'];
         
-        $this->conn->query("UPDATE  portefeuille set qte=:qte where user_id =:user_id AND crypto_id=:crypto_id ");
+        $this->conn->query("UPDATE  portefeuille set soldusdt =:qte where user_id =:user_id AND crypto_id=:crypto_id ");
         }else{
-            $this->conn->query("INSERT INTO portefeuille (user_id,crypto_id,qte) VALUES (:user_id,:crypto_id,:qte)"); 
+            $this->conn->query("INSERT INTO portefeuille (user_id,crypto_id,soldusdt ) VALUES (:user_id,:crypto_id,:qte)"); 
         }
 
         $this->conn->bind(':user_id', 1);
